a91795e009e3c4b22f4b25ac56867ab9c218d9b0
FRAHHH <gamer.tick333@gmail.com>
2026-09-02 03:10:22 +1000

    EJS

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-import express from 'express'  
-
+const express = require('express');
+const path = require('path');
 const app = express()
 
+const PORT = 3000;
+//configurar ejs
+app.set('view engine', 'ejs');
+
+
+
+//archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//rutas
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('index');
 })
 
-app.listen(3000, () => {
+app.get('/servicios', (req, res) => {
+  res.render('servicios');
+})
+
+app.get('/nosotros', (req, res) => {
+  res.render('nosotros');
+})
+app.get('/contacto', (req, res) => {
+  res.render('contacto');
+})
+
+app.listen(PORT, () => {
   console.log('Server is running on http://localhost:3000')
 })
